305641ec86b189dc64ba4fb6819dbb7436ac4803
郑俊鑫 <18810544291@163.com>
2017-08-20 08:31:34 +0800

    完成首页的数据填充

--- server/src/controllers/home.js
+++ server/src/controllers/home.js
@@ -1,14 +1,75 @@
 const Banner = require('../proxy/banner')
+const Category = require('../proxy/category')
+const Good = require('../proxy/good')
 
-exports.index = (req, res) => {
-    
-    Promise.all([Banner.getBanners()])
-        .then(([banners]) => {
+const prefixImageUrl = imageUrl => `http://localhost:8080${imageUrl}`
+
+// 异步获取 categories
+// 异步获取 cateogries 下的 goods
+
+exports.index = async (req, res) => {
+    const banners = Banner.getBanners()
+    const categories = Category.list({ parent_id: 0 })
+    const categoryList = Category.list({ parent_id: 0 })
+        .then(categories => {
+            const all = []
+            for (let i = 0; i < categories.length; i++) {
+                const category = categories[i]
+                const promise = Good.getByCategory(category.id)
+                    .limit(6)
+                    .then(goods => {
+                        const goodsList = goods.map(good => {
+                            good.list_pic_url = prefixImageUrl(good.list_pic_url)
+                            return good
+                        })
+
+                        return {
+                            name: category.name,
+                            id: category.id,
+                            goodsList: goodsList,
+                        }
+                    })
+                all.push(promise)
+            }
+            
+            return Promise.all(all)
+        })
+
+    Promise.all([banners, categories, categoryList])
+        .then(([banners, categories, categoryList]) => {
+            banners = banners.map(banner => {
+                banner.image_url = prefixImageUrl(banner.image_url)
+                return banner
+            })
+            categories = categories.map(category => {
+                category.icon_url = prefixImageUrl(category.icon_url)
+                return category
+            })
+
             const result = {
                 errno: 0,
                 errmsg: '',
                 data: {
-                    banners,
+                    banner: banners,
+                    brandList: [
+                        {
+                            name: '居家',
+                            id: 11,
+                            goodsList: [
+                                {
+                                    id: 2,
+                                    list_pic_url: '',
+                                    name: '名字',
+                                    retail_price: 23,
+                                }
+                            ]
+                        }
+                    ],
+                    categoryList: categoryList,
+                    channel: categories,
+                    hotGoodsList: [],
+                    newGoodsList: [],
+                    topicList: [],
                 }
             }
             res.send(result)
--- server/src/proxy/good.js
+++ server/src/proxy/good.js
@@ -2,7 +2,11 @@ const Model = require('../models/good')
 const Base = require('./base')
 
 class Good extends Base {
-
+    getByCategory = categoryId => {
+        return this.model.find({
+            category_id: categoryId
+        })
+    }
 }
 const proxy = new Good(Model)
 
